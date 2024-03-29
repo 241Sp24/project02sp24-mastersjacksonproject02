71eff677d957c03e889aa7521307c35997af2fe7
danyellejackson18 <158313743+danyellejackson18@users.noreply.github.com>
2024-03-29 10:43:35 -0500

    studentFees
    worked on studentFees class

--- src/studentdriver/StudentFees.java
+++ src/studentdriver/StudentFees.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package studentdriver;
 
-/**
- *
- * @author s544842
- */
 public abstract class StudentFees {
+private String studentName;
+private int studentID; 
+private boolean isEnrolled; 
+private int CREDIT_PER_COURSE = 3; 
+private double PER_CREDIT_FEE = 543.50;
 
+
+
+public StudentFees(String studentName, int studentID, boolean isEnrolled){
+    
+}
+public String getStudentName(){
+    
+}
+public int getStudentID(){
+    
+}
+public boolean isEnrolled(){
+}
+public int getCREDIT_PER_COURSE(){
+    
+}
+public double getPER_CREDIT_FEE(){
+    
+}
+public void setStudentName(String studentName){
+    
+}
+public void setStudentID(int studentID){
+    
+}
+public void setIsEnrolled(boolean isEnrolled){
+    
+}
+public double getPayableAmount(){
+    
+}
+public String toString(){
+    
+}
 }
