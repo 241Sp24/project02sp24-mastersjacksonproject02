888746a651fccda98d23462ad14cf7384516d9d2
danyellejackson18 <158313743+danyellejackson18@users.noreply.github.com>
2024-04-02 15:52:26 -0500

    Completed methods
    Completed most of the methods

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -1,11 +1,30 @@
 
 package studentdriver;
-
+import java.util.*;
 public class StudentDriver {
 
     
     public static void main(String[] args) {
         System.out.println("Project02");
     }
+    
+    StudentFees[] students = new StudentFees[12];
 
+    System.out.println("**********Undergraduate students list**********");
+
+    
+    
+
+    
+    System.out.println("***********Graduate students list**********");
+    
+    
+    
+    
+    
+    System.out.println("**********Online students list**********");
+    
 }
+
+
+
