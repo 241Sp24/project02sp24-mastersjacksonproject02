a80562585d5600edd7779b08008282e987a79c03
danyellejackson18 <158313743+danyellejackson18@users.noreply.github.com>
2024-04-02 18:13:23 -0500

    Actually worked on methods
    Completed most of the methods

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -8,23 +8,34 @@ private boolean isGraduateAssistant;
 private String graduateAssistantType;
 private double ADDITIONAL_FEES = 654.45;
 
-public void GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, String graduateAssistantType, int coursesEnrolled){
-    
+public  GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, String graduateAssistantType, int coursesEnrolled){
+    super(studentName, studentID, isEnrolled);
+    this.coursesEnrolled = coursesEnrolled;
+    this.isGraduateAssistant = isGraduateAssistant;
+    this.graduateAssistantType = graduateAssistantType;
 }
-public void GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, int coursesEnrolled){
-    
+public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, int coursesEnrolled){
+    super(studentName, studentID, isEnrolled);
+    this.coursesEnrolled = coursesEnrolled;
+    this.isGraduateAssistant = isGraduateAssistant;
 }
 public boolean isGraduateAssistant(){
-    
+    return isGraduateAssistant;
 }
 public int getCoursesEnrolled(){
-    
+    return coursesEnrolled;
 }
 public double getPayableAmount(){
-    
+    //double tuitionFee = calculateTuitionFee();
+    double totalFee = tuitionFee + ADDITIONAL_FEES;
+        if (isGraduateAssistant) {
+            if ("full".equalsIgnoreCase(graduateAssistantType)) {
+                totalFee -= tuitionFee;
+            } else if ("half".equalsIgnoreCase(graduateAssistantType)) {
+                totalFee -= tuitionFee / 2;
 }
 public String toString(){
-    
+        return ("Student name" + super.getStudentName()  + "\nStudent id: " + super.getStudentID() + "\nEnrolled: " + this.isEnrolled() + "\nGraduate assistant: " + this.isGraduateAssistant()+ "Graduate assistant type: " + this.graduateAssistantType + "\nCourses enrolled: " + coursesEnrolled + "\nPayable Amount: " + this.getPayableAmount());
 }
 }
 
--- src/studentdriver/OnlineStudent.java
+++ src/studentdriver/OnlineStudent.java
@@ -6,13 +6,15 @@ public class OnlineStudent extends StudentFees {
 private int noOfMonths;
 private double MONTHLY_FEE = 1245.25;
 
-public void OnlineStudent(String studentName, int studentID, boolean isEnrolled, int onOfMonths){
-    
+public OnlineStudent(String studentName, int studentID, boolean isEnrolled, int onOfMonths){
+    super(studentName, studentID, isEnrolled);
+    this.noOfMonths = noOfMonths;
+
 }
 public double getPayableAmount(){
-    
+    return MONTHLY_FEE * noOfMonths;
 }
 public String toString(){
-    
+        return ("Student name" + super.getStudentName()  + "\nStudent id: " + super.getStudentID() + "\nEnrolled: " + this.isEnrolled() + "No of months: " + noOfMonths + "\nPayable Amount: " + this.getPayableAmount());
 }
 }
