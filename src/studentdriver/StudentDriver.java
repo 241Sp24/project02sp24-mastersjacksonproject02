b1fc0da5de3e7c8e4ebc758795d1490006e45041
s544842 <158311641+s544842@users.noreply.github.com>
2024-04-03 13:49:31 -0500

    started loops

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -10,10 +10,24 @@ public class StudentDriver {
 
         StudentFees[] students = new StudentFees[12];
         Scanner input = new Scanner(new File("input.csv"));
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Enter the number of Undergraduate Students: ");
+        int nougS = user_input.nextInt();
+        System.out.println("Enter the number of Graduate Students: ");
+        int nogS = user_input.nextInt();
+        System.out.println("Enter the number of Online Students: ");
+        int nooS = user_input.nextInt();
         //create seperate for loops based off of user input, creates student objects;assigns to students[]
-        String[] nl = input.nextLine().split(",", 6);
-        //UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled)
-        students[0] = new UGStudent((nl[1]), (Integer.parseInt(nl[0])), (Boolean.parseBoolean(nl[2])), (Boolean.parseBoolean(nl[4])), (Double.parseDouble(nl[3])), (Integer.parseInt(nl[5])));
+        for (int i = 0; i < nougS; i++) {
+            //undergraduate student loop
+            String[] nl = input.nextLine().split(",", 6);
+            students[i] = new UGStudent((nl[1]), (Integer.parseInt(nl[0])), (Boolean.parseBoolean(nl[2])), (Boolean.parseBoolean(nl[4])), (Double.parseDouble(nl[3])), (Integer.parseInt(nl[5])));
+        }
+        //graduate student loop
+        for (int i = 0; i < nogS; i++) {
+            String[] nl = input.nextLine().split(",", 6);
+            students[i + nougS] = new GraduateStudent((nl[1]), (Integer.parseInt(nl[0])), (Boolean.parseBoolean(nl[2])), (Boolean.parseBoolean(nl[3])), (nl[4]), (Integer.parseInt(nl[5])));
+        }
         System.out.println("**********Undergraduate students list**********");
 
         System.out.println("***********Graduate students list**********");
